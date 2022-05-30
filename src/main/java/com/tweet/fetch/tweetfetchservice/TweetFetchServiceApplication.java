d3c659b0185ddd1aa7d6f73044ffec629d6734b2
Anjani Jha <anjanikumarjha1991@gmail.com>
2022-05-30 10:07:54 +0530

    without eureka and google oauth

--- src/main/java/com/tweet/fetch/tweetfetchservice/TweetFetchServiceApplication.java
+++ src/main/java/com/tweet/fetch/tweetfetchservice/TweetFetchServiceApplication.java
@@ -2,12 +2,10 @@ package com.tweet.fetch.tweetfetchservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
-@EnableWebSecurity
+//@EnableWebSecurity
 public class TweetFetchServiceApplication {
 
     public static void main(String[] args) {
